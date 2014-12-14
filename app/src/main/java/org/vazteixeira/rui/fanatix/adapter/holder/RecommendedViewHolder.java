464bafa1f2977647e1b3faa5e48e10797097af4e
Rui Teixeira <rui@vazteixeira.org>
2014-12-14 17:48:57 +0000

    adding friends view holders

---
+++ app/src/main/java/org/vazteixeira/rui/fanatix/adapter/holder/RecommendedViewHolder.java
@@ -0,0 +1,19 @@
+package org.vazteixeira.rui.fanatix.adapter.holder;
+
+import android.widget.TextView;
+
+/**
+ * Created by rmvt on 14/12/14.
+ */
+public class RecommendedViewHolder extends FriendViewHolder {
+
+    private TextView teamTextView;
+
+    public TextView getTeamTextView() {
+        return teamTextView;
+    }
+
+    public void setTeamTextView(TextView teamTextView) {
+        this.teamTextView = teamTextView;
+    }
+}
