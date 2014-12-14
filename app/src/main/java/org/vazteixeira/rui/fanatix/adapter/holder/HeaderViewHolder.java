0ee2324efa16ced266260b6ea0e65c2f17c7a5f2
Rui Teixeira <rui@vazteixeira.org>
2014-12-14 17:31:41 +0000

    add header view holder

---
+++ app/src/main/java/org/vazteixeira/rui/fanatix/adapter/holder/HeaderViewHolder.java
@@ -0,0 +1,19 @@
+package org.vazteixeira.rui.fanatix.adapter.holder;
+
+import android.widget.TextView;
+
+/**
+ * Created by rmvt on 14/12/14.
+ */
+public class HeaderViewHolder {
+
+    private TextView titleTextView;
+
+    public TextView getTitleTextView() {
+        return titleTextView;
+    }
+
+    public void setTitleTextView(TextView titleTextView) {
+        this.titleTextView = titleTextView;
+    }
+}
